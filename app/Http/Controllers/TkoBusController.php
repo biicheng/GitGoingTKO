8b5544424f6cb07c16e40fd71c5ec74ef47fe731
Nick <fish970628@gmail.com>
2018-09-14 14:36:47 +0800

    Nick0236

---
+++ app/Http/Controllers/TkoBusController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TkoBusController extends Controller
+{
+	public function index()
+	{
+		return 'Nickさんは髙雄がすきです';
+	}
+}
